4038f22c39daf72a4cf95eebff4aabd8520cfd69
emirozer <7emirozer@gmail.com>
2026-08-17 15:41:08 +0300

    feat(vehicle): VehicleController REST endpoints skeleton

--- services/vehicle-service/src/main/java/com/yaltes/vehicle_service/controller/VehicleController.java
+++ services/vehicle-service/src/main/java/com/yaltes/vehicle_service/controller/VehicleController.java
@@ -1,9 +1,34 @@
 package com.yaltes.vehicle_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yaltes.vehicle_service.entity.Vehicle;
+import com.yaltes.vehicle_service.repository.VehicleRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    // POST
+    @PostMapping
+    public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+    // GET
+    @GetMapping
+    public List<Vehicle> findAll(){ return vehicleRepository.findAll(); }
+    // PATCH
+    @PatchMapping
+    public Vehicle update(@RequestBody Vehicle vehicle) {return vehicleRepository.save(vehicle); }
+    // DELETE
+    @DeleteMapping
+    public void delete(@RequestBody Vehicle vehicle) {}
+
 }
