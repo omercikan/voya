98ca1e55beb0dfd6390ccb57f0158793e1c13963
omercikan <omercikan36@gmail.com>
2026-08-20 09:56:42 +0300

    fix(auth): enable CORS on login endpoint
    - Add @CrossOrigin to AuthController to allow the frontend to call
      the login endpoint directly on identity-service, in addition to
      routing through the gateway

--- services/identity-service/src/main/java/com/yaltes/identity_service/controller/AuthController.java
+++ services/identity-service/src/main/java/com/yaltes/identity_service/controller/AuthController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
