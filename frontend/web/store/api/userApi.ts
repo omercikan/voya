e0424d4efe3b3d3203e4515664d58be5caa0f188
omercikan <omercikan36@gmail.com>
2026-08-22 23:29:46 +0300

    feat(frontend): add user status api endpoints
    - add endpoint for fetching users
    - add endpoint for updating user account status
    - invalidate user cache after status updates

--- frontend/web/store/api/userApi.ts
+++ frontend/web/store/api/userApi.ts
@@ -1,4 +1,4 @@
-import { User } from "@/types/user";
+import { AccountStatus, User } from "@/types/user";
 import { baseApi } from "./baseApi";
 
 const userApi = baseApi.injectEndpoints({
@@ -8,9 +8,31 @@ const userApi = baseApi.injectEndpoints({
         method: "GET",
         url: "/api/users/me",
       }),
+    }),
+
+    getUsers: builder.query<{ data: User[]; success: boolean }, void>({
+      query: () => ({
+        method: "GET",
+        url: "/api/users",
+      }),
       providesTags: ["User"],
+    }),
+
+    updateStatus: builder.mutation<
+      void,
+      { userId: number; status: AccountStatus }
+    >({
+      query: ({ userId, status }) => ({
+        method: "PATCH",
+        url: `/api/users/${userId}/status`,
+        body: {
+          status,
+        },
+      }),
+      invalidatesTags: ["User"],
     }),
   }),
 });
 
-export const { useGetUserQuery } = userApi;
+export const { useGetUserQuery, useGetUsersQuery, useUpdateStatusMutation } =
+  userApi;
