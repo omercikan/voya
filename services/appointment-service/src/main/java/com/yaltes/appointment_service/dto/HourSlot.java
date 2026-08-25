32ee817aec529fdc08fa0d587f68d1a07881f881
Egehan <egehankirmizi7264@gmail.com>
2026-08-25 16:27:50 +0300

    **feat: add HourSlot DTO**
    * Added `HourSlot` DTO for representing hourly availability
    * Added time slot and free vehicle count fields
    * Added constructors, getters, and setters

---
+++ services/appointment-service/src/main/java/com/yaltes/appointment_service/dto/HourSlot.java
@@ -0,0 +1,30 @@
+package com.yaltes.appointment_service.dto;
+
+public class HourSlot {
+    private String time;
+    private int free;
+
+    public HourSlot() {
+    }
+
+    public HourSlot(String time, int free) {
+        this.time = time;
+        this.free = free;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getFree() {
+        return free;
+    }
+
+    public void setFree(int free) {
+        this.free = free;
+    }
+}
