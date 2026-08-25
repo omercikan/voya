90d96dec2d5abfab8175dbeb8b07161558aa66c7
Egehan <egehankirmizi7264@gmail.com>
2026-08-25 16:27:40 +0300

    **feat: add DayAvailability DTO**
    * Added `DayAvailability` DTO for daily vehicle availability
    * Added date and availability status fields
    * Added minimum free vehicle count
    * Added hourly availability information

---
+++ services/appointment-service/src/main/java/com/yaltes/appointment_service/dto/DayAvailability.java
@@ -0,0 +1,43 @@
+package com.yaltes.appointment_service.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class DayAvailability {
+    private LocalDate date;
+    private String status;
+    private int minFreeVehicles;
+    private List<HourSlot> hourly;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getMinFreeVehicles() {
+        return minFreeVehicles;
+    }
+
+    public void setMinFreeVehicles(int minFreeVehicles) {
+        this.minFreeVehicles = minFreeVehicles;
+    }
+
+    public List<HourSlot> getHourly() {
+        return hourly;
+    }
+
+    public void setHourly(List<HourSlot> hourly) {
+        this.hourly = hourly;
+    }
+}
