d1e63f40c354ef9b0e2e52e522ff2599a0fa56dc
omercikan <omercikan36@gmail.com>
2026-08-26 12:48:34 +0300

    feat(appointment): add appointment details step

--- frontend/web/components/dashboard/employee/appointment/AppoinmentDetails.tsx
+++ frontend/web/components/dashboard/employee/appointment/AppoinmentDetails.tsx
@@ -42,6 +42,9 @@ const AppoinmentDetails = () => {
               rows={4}
               className="flex min-h-15 w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
               placeholder="Örn: Müşteri tesisine ziyaret"
+              onChange={(e) =>
+                dispatch(setAppointment({ note: e.target.value }))
+              }
             />
           </div>
         </div>
