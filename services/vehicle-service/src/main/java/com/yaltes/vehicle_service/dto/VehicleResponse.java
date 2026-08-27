d7d4b45a665078fa6644b5809e03f1e32f797592
emirozer <7emirozer@gmail.com>
2026-08-27 10:25:36 +0300

    refactor: standardize id types to Long across vehicle entities

--- services/vehicle-service/src/main/java/com/yaltes/vehicle_service/dto/VehicleResponse.java
+++ services/vehicle-service/src/main/java/com/yaltes/vehicle_service/dto/VehicleResponse.java
@@ -4,7 +4,7 @@ import com.yaltes.vehicle_service.enums.AvailabilityStatus;
 
 public class VehicleResponse{
 
-    private Integer id;
+    private Long id;
     private String plate;
     private String brand;
     private String model;
@@ -17,8 +17,8 @@ public class VehicleResponse{
 
     // Getter-Setter
 
-    public Integer getId() {return id;}
-    public void setId(Integer id) {this.id = id;}
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
 
     public String getPlate() {return plate;}
     public void setPlate(String plate) {this.plate = plate;}
